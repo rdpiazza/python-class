4d26457228ad18d1e39d390013f9536731b8270b
rdpiazza <rebecca.piazza@gsa.gov>
2015-07-29 17:54:23 -0400

    Changed STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,5 +99,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = '/Users/rebeccapiazza/learn/static'
+STATIC_ROOT = '/home/rdpiazza/static'
 STATIC_URL = '/static/'
